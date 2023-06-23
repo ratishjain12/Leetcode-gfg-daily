bec194f60d26f4fd69f9bcd4d660a8e5609996f1
ratishjain <ratishjain6@gmail.com>
2023-06-23 23:22:58 +0530

    gfg daily

---
+++ gfg/task-scheduler-greedy.cpp
@@ -0,0 +1,17 @@
+int leastInterval(int N, int K, vector<char> &tasks) {
+    // code here
+    int freq[26] = {0};
+    int maxfreq = INT_MIN;
+    for(int i = 0;i<tasks.size();i++){
+        freq[tasks[i]-'A']++;
+        maxfreq = max(maxfreq,freq[tasks[i] - 'A']);
+    }
+    
+    int ans = (K+1)*(maxfreq-1);
+    for(int i = 0;i<26;i++){
+        if(freq[i] == maxfreq){
+            ans++;
+        }
+    }
+    return max(N,ans);
+}
