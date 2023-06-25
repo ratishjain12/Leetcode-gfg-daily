34218163c2514d2e9a381b98a6daf9d777dc8314
ratishjain <ratishjain6@gmail.com>
2023-06-26 00:11:08 +0530

    lc daily

---
+++ leetcode/count-possible-routes(DP and lc hard).cpp
@@ -0,0 +1,26 @@
+int mod = 1e9+7;
+int solve(vector<vector<int>>&dp,vector<int>& locations,int current, int finish, int fuel,int n){
+    if(fuel<0){
+        return 0;
+    }
+
+    if(dp[current][fuel] != -1){
+        return dp[current][fuel];
+    }
+    
+    int ans = 0;
+    if(current == finish) ans++;
+    for(int next = 0;next<n;next++){
+        if(next!=current){
+            ans = (ans%mod + solve(dp,locations,next,finish,fuel - abs(locations[current] - locations[next]),n)%mod)%mod;
+        }
+    }
+
+    return dp[current][fuel] = ans%mod;
+}
+int countRoutes(vector<int>& locations, int start, int finish, int fuel) {                
+    int n = locations.size();    
+
+    vector<vector<int>> dp(n, vector<int>(fuel + 1, -1));
+    return solve(dp,locations,start,finish,fuel,n);
+}
